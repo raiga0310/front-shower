6c4beef458159fff92f6924b9384b9c7f6e86925
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-07-17 12:02:57 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Header from './lib/Header.svelte';
+    import Footer from './lib/Footer.svelte';
+    import ShowerList from './lib/ShowerList.svelte';
+</script>
+
+<main>
+  <Header />
+  <ShowerList />
+  <Footer />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 90vw;
+    max-height: 80vh;
+    margin: 0 auto;
+  }
+</style>
